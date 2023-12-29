4dc932624590ece13cc82e34c59313ac12d3b12f
Jonas <btxtiger@icloud.com>
2023-12-30 00:06:05 +0100

    Fixes

--- build/php-fpm/entrypoint.sh
+++ build/php-fpm/entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+shutdownHandler() {
+   echo "STOP signal received..."
+   # Add any cleanup or graceful shutdown tasks here
+   exit 0
+}
+trap 'shutdownHandler' TERM INT
+
+cd /app || exit 1
+
 # Check if the Laravel application is not present in /app
 if [ ! -f "/app/artisan" ]; then
     echo "No Laravel application found in /app. Exiting..."
@@ -8,21 +17,37 @@ else
     echo "Laravel application found in /app."
 fi
 
-# Start Redis
-echo "Starting Redis..."
-redis-server &
+# Start Redis if not running
+if ! pgrep "redis-server" > /dev/null; then
+   echo "Starting Redis..."
+   redis-server &
+else
+    echo "Redis-server is already running."
+fi
 
-# Start Cron
-echo "Starting Cron..."
-crond
+# Start Cron if not running
+if ! pgrep "crond" > /dev/null; then
+   echo "Starting Cron..."
+   crond &
+else
+    echo "Cron is already running."
+fi
 
-# Start PHP-FPM
-echo "Starting PHP-FPM..."
-php-fpm
+# Start PHP-FPM if not running
+if ! pgrep "php-fpm" > /dev/null; then
+   echo "Starting PHP-FPM..."
+   php-fpm &
+else
+    echo "PHP-FPM is already running."
+fi
 
-# Start Nginx
-echo "Starting Nginx..."
-nginx -g 'daemon off;'
+# Start Nginx if not running
+if ! pgrep "nginx" > /dev/null; then
+   echo "Starting Nginx..."
+   nginx &
+else
+    echo "Nginx is already running."
+fi
 
 # Start Supervisor
 # echo "Starting Supervisor..."
@@ -49,41 +74,83 @@ if ! pgrep "nginx" > /dev/null; then
     failed=true
 fi
 
-if ! pgrep "supervisord" > /dev/null; then
-    echo "Supervisor failed to start."
-    failed=true
-fi
+#if ! pgrep "supervisord" > /dev/null; then
+#    echo "Supervisor failed to start."
+#    failed=true
+#fi
 
 if [ "$failed" = true ]; then
     echo "One or more services failed to start. Exiting..."
     exit 1
 fi
 
-# Composer
-if [ -f "/app/composer.json" ]; then
-    echo "Installing composer dependencies..."
-    cd /app && composer install --no-dev --optimize-autoloader --no-interaction --no-progress --no-suggest --prefer-dist
-else
-   echo "No composer.json found in /app. Shutting down..."
-   exit 1
-fi
+# Create cache paths: mkdir -p storage/framework/{sessions,views,cache}
+echo "Creating cache paths..."
+mkdir -p storage/framework/sessions
+mkdir -p storage/framework/views
+mkdir -p storage/framework/cache
+echo "============================"
+echo "===  Cache paths created ==="
+echo "============================"
 
-# NPM Build
-if [ -f "/app/package.json" ]; then
-    echo "Installing npm dependencies..."
-    cd /app && npm ci --no-audit
+# Fix storage permissions
+echo "Fixing storage permissions..."
+chmod -R 755 storage
+chown -R www-data:www-data storage
+echo "============================"
+echo "===  Permissions fixed   ==="
+echo "============================"
 
-    echo "Building npm assets..."
-    cd /app && npm run build
-else
-   echo "No package.json found in /app. Shutting down..."
-   exit 1
-fi
+# Optimize Laravel
+echo "Optimizing Laravel..."
+#php artisan config:cache
+#php artisan route:cache
+#php artisan view:cache
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+echo "============================"
+echo "===  Laravel optimized   ==="
+echo "============================"
+
+crond start -f -l 8 &
+echo "============================"
+echo "=== Cron service started ==="
+echo "============================"
+
+echo "Installing Composer..."
+composer install --no-dev --optimize-autoloader --no-interaction --no-progress --no-suggest --prefer-dist
+echo "=========================="
+echo "=== Composer installed ==="
+echo "=========================="
+
+echo "Installing NPM..."
+npm ci --no-audit
+echo "=========================="
+echo "===   NPM installed    ==="
+echo "=========================="
+
+echo "Building NPM..."
+npm run build
+echo "=========================="
+echo "===     NPM built      ==="
+echo "=========================="
 
 # Migrate database
 echo "Migrating database..."
-cd /app && php artisan migrate --force
+php artisan migrate --force
+echo "============================"
+echo "=== Migrations completed ==="
+echo "============================"
 
-echo "========================================"
-echo "Ready."
-echo "========================================"
+echo "alias pa=\"php artisan\"" > ~/.bashrc
+
+echo "============================"
+echo "===      PHP READY       ==="
+echo "============================"
+
+# wait forever
+while true; do
+   tail -f /dev/null &
+   wait ${!}
+done
